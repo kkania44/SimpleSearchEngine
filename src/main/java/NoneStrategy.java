518d605c1b1f8cddb69a1ebb1f5670cbda39541e
kkania44 <kaniakrzysztof99@gmail.com>
2020-03-10 18:19:09 +0100

    NoneStrategy fix2 and new tests

--- src/main/java/NoneStrategy.java
+++ src/main/java/NoneStrategy.java
@@ -1,13 +1,19 @@
 import java.util.*;
 import java.util.function.Predicate;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 public class NoneStrategy implements SearchStrategy {
+
+    Logger logger = Logger.getLogger(NoneStrategy.class.getName());
 
     @Override
     public Set<Integer> search(String[] searchQuery, Map<String, Set<Integer>> occurrences) {
         Set<Integer> matchedIndexes = new HashSet<>();
         occurrences.values().forEach(matchedIndexes::addAll);
+
+        logger.setLevel(Level.INFO);
 
         if(searchQuery.length == 1) {
             matchedIndexes.removeAll(occurrences.get(searchQuery[0]));
@@ -16,7 +22,9 @@ public class NoneStrategy implements SearchStrategy {
                 if (occurrences.containsKey(search)) {
                     Set<Integer> toRemove = occurrences.get(search);
                     toRemove.retainAll(matchedIndexes);
+//                    logger.info(toRemove.toString());
                     matchedIndexes.removeAll(toRemove);
+//                    logger.info(matchedIndexes.toString());
                     }
                 }
             }
--- src/test/java/NoneStrategyTest.java
+++ src/test/java/NoneStrategyTest.java
@@ -12,23 +12,38 @@ class NoneStrategyTest {
 
     @BeforeEach
     void setUp() {
-        occurrences = SearchEngine.mapData(Arrays.asList("One two three", "Two three four", "One five six", "Four Seven eight"));
+        occurrences = SearchEngine.mapData(Arrays.asList("One two three", "Two three four", "One five six", "four seven eight"));
     }
 
     @Test
     void shouldFindOneMatchingLine() {
         // given
-        String[] search = "three four seven".split("\\s+");
-        Set<Integer> expected = Set.of(2);
-        int sizeEx = expected.size();
+        String[] search1 = "three four seven".split("\\s+");
+        String[] search2 = "one eight".split("\\s+");
+        Set<Integer> expected1 = Set.of(2);
+        Set<Integer> expected2 = Set.of(1);
 
         // when
-        Set<Integer> actual = searchStrategy.search(search, occurrences);
-        int sizeAc = actual.size();
+        Set<Integer> actual1 = searchStrategy.search(search1, occurrences);
+        Set<Integer> actual2 = searchStrategy.search(search2, occurrences);
 
         // then
-        assertEquals(sizeEx, sizeAc);
-        assertTrue(actual.containsAll(expected));
+        assertEquals(expected1.size(), actual1.size());
+        assertEquals(expected2.size(), actual2.size());
+        assertTrue(actual1.containsAll(expected1));
+        assertTrue(actual2.containsAll(expected2));
+
+    }
+
+    @Test
+    void shouldFindTwoMatchingLines() {
+        String[] search3 = "four seven".split("\\s+");
+        Set<Integer> expected3 = Set.of(0,2);
+
+        Set<Integer> actual3 = searchStrategy.search(search3, occurrences);
+
+        assertEquals(expected3.size(), actual3.size());
+        assertTrue(actual3.containsAll(expected3));
     }
 
     @Test
